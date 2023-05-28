b162693d9ca5ce265d2be787e1f4446b227027b7
ryodgie <ryodgie@gmail.com>
2023-05-28 15:24:06 +0800

    feat: add info modal

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -14,16 +14,54 @@
 
       <div class="flex gap-3 flex-1 justify-end">
         <i
+          @click="toggleModal"
           class="fa-solid fa-circle-info hover:text-sky-300 duration-150 cursor-pointer"
         ></i>
         <i
           class="fa-solid fa-plus hover:text-sky-300 duration-150 cursor-pointer"
         ></i>
       </div>
+      <BaseModal :modalActive="modalActive" @close-modal="toggleModal">
+        <div class="text-black">
+          <h1 class="text-lg mb-1">About:</h1>
+          <p class="mb-4">
+            The Local Weather allows you to track the current and future weather
+            of cities of your choosing.
+          </p>
+          <h2 class="text-lg">How it works:</h2>
+          <ol class="list-decimal list-inside mb-4">
+            <li>
+              Search for your city by entering the name into the search bar.
+            </li>
+            <li>
+              Select a city within the results, this will take you to the
+              current weather for your selection.
+            </li>
+            <li>
+              Track the city by clicking on the "+" icon in the top right. This
+              will save the city to view at a later time on the home page.
+            </li>
+          </ol>
+
+          <h2 class="text-lg">Removing a city</h2>
+          <p>
+            If you no longer wish to track a city, simply select the city within
+            the home page. At the bottom of the page, there will be am option to
+            delete the city.
+          </p>
+        </div>
+      </BaseModal>
     </nav>
   </header>
 </template>
 
 <script setup>
+import { ref } from "vue";
 import { RouterLink } from "vue-router";
+import BaseModal from "./BaseModal.vue";
+
+const modalActive = ref(null);
+const toggleModal = () => {
+  modalActive.value = !modalActive.value;
+};
 </script>
