ccfb83565f3c44e87621998b730db0d3b5229532
ryodgie <ryodgie@gmail.com>
2023-05-28 16:06:16 +0800

    feat: add city search

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,54 @@
 <template>
-  <div></div>
+  <main class="font-['Sora'] text-sm container text-white">
+    <div class="pt-4 mb-8 relative">
+      <input
+        @input="getSearchResults"
+        type="text"
+        v-model="searchQuery"
+        placeholder="Search for a city or state"
+        class="py-2 px-1 w-full bg-transparent border-b focus:border-white focus:outline-none"
+      />
+      <ul
+        v-if="mapboxSearchResults"
+        class="absolute bg-weather-secondary text-white w-full shadow-md py2 px-1 top-[66px]"
+      >
+        <li
+          v-for="searchResult in mapboxSearchResults"
+          :key="searchResult.id"
+          class="py-2 cursor-pointer"
+        >
+          {{ searchResult.place_name }}
+        </li>
+      </ul>
+    </div>
+  </main>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
+
+const mapboxAPIKey =
+  "pk.eyJ1Ijoiam9obmtvbWFybmlja2kiLCJhIjoiY2t5NjFzODZvMHJkaDJ1bWx6OGVieGxreSJ9.IpojdT3U3NENknF6_WhR2Q";
+const searchQuery = ref("");
+const queryTimeout = ref(null);
+const mapboxSearchResults = ref(null);
+
+const getSearchResults = () => {
+  clearTimeout(queryTimeout.value); // Lazy search - clear default timeout when user enter a letter to search bar and when user stop typing this won't run again then reach out to lazy location api
+  queryTimeout.value = setTimeout(async () => {
+    if (searchQuery.value !== "") {
+      const result = await axios.get(
+        `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}&types=place`
+      );
+      mapboxSearchResults.value = result.data.features;
+      // console.log(mapboxSearchResults.value);
+      return;
+    }
+    //This never gets executed if search is true
+    mapboxSearchResults.value = null;
+  }, 300);
+};
+</script>
 
 <style lang="scss" scoped></style>
